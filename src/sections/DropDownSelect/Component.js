17188eadb42a1ba774441e52beed875264236aaa
withewolf-dev <itskashyap26@gmail.com>
2020-07-04 22:36:04 +0700

    first commit

---
+++ src/sections/DropDownSelect/Component.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import {useStyles,BootstrapInput} from './styles'
+
+export default function CustomizedSelects() {
+  const classes = useStyles();
+  const [age, setAge] = React.useState('');
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  return (
+    <div>
+      <FormControl className={classes.margin}>
+        <InputLabel htmlFor="demo-customized-select-native">issue</InputLabel>
+        <NativeSelect
+          id="demo-customized-select-native"
+          value={age}
+          onChange={handleChange}
+          input={<BootstrapInput />}
+        >
+          <option aria-label="None" value="" />
+          <option value={10}>september</option>
+          <option value={20}>october</option>
+          <option value={30}>november</option>
+        </NativeSelect>
+      </FormControl>
+    </div>
+  );
+}
